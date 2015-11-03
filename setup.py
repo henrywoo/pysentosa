26bf26df637b813e945d63bba2f722b661ca08e7
Wu Fuheng <henry.woo@outlook.com>
2015-11-02 22:54:13 -0500

    add requirements

--- setup.py
+++ setup.py
@@ -2,14 +2,12 @@
 # -*- coding: utf-8 -*-
 
 from setuptools import setup, find_packages
-import codecs, os
+import codecs
+import os
 
 def read(fname):
-    """
-    定义一个read方法，用来读取目录下的长描述
-    我们一般是将README文件中的内容读取出来作为长描述，这个会在PyPI中你这个包的页面上展现出来，
-    你也可以不用这个方法，自己手动写内容即可，
-    PyPI上支持.rst格式的文件。暂不支持.md格式的文件，<BR>.rst文件PyPI会自动把它转为HTML形式显示在你包的信息页面上。
+    """ Read long description given a file name
+    PyPI supports .rst but not .md. rst file will be displayed in PyPI in HTML format。
     """
     return codecs.open(os.path.join(os.path.dirname(__file__), fname)).read()
 
@@ -21,7 +19,7 @@ setup(
     packages = find_packages(),
     package_dir ={'pysentosa': 'pysentosa'},
     package_data={'pysentosa': ['sentosa_.so']},
-    description = "Pysentosa - Python API for sentosa trading system",
+    description = "pysentosa - Python API for sentosa trading system",
     long_description = LONG_DESCRIPTION,
     author = "Wu Fuheng",
     author_email = "henry.woo@outlook.com",
